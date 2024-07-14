4dedcd6fe895111ebdd151cd83ab13d7e85bb5a7
Rushabh Mehrotra <rushabhnm10@gmail.com>
2024-07-13 19:37:04 -0500

    adding the initial files

---
+++ 1.HelloSpark.py
@@ -0,0 +1,16 @@
+import pyspark
+
+from pyspark.sql import SparkSession
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("Hello Spark").master("local[2]").getOrCreate()
+
+    datalist = [("Rushabh",25),
+                ("Uma", 51),
+                ("Navin",58)]
+
+    df = spark.createDataFrame(datalist).toDF("Name","Age")
+
+    df.show()
+
